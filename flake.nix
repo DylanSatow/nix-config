eb553b55e746f7b6cb0124099e97b9b97aedeab1
DylanSatow <dylansatow531@gmail.com>
2025-07-12 22:20:21 -0700

    Added cask functionality

--- flake.nix
+++ flake.nix
@@ -5,16 +5,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask }:
 
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#dylanix
     darwinConfigurations."dylanix" = nix-darwin.lib.darwinSystem {
-	modules = [ ./mac-configuration.nix ];
-	specialArgs = { inherit self; };
+	modules = [ ./mac-configuration.nix nix-homebrew.darwinModules.nix-homebrew ];
+	specialArgs = { inherit self homebrew-core homebrew-cask; };
     };
   };
 }
--- mac-configuration.nix
+++ mac-configuration.nix
@@ -1,31 +1,56 @@
-{ pkgs, self, ... }: {
-	# List packages installed in system profile. To search by name, run:
-	# $ nix-env -qaP | grep wget
-	environment.systemPackages =
-	[ 
-		pkgs.vim
-		pkgs.neovim
-	];
-	system.primaryUser = "dylan"; # Set main user for dock
-	
-	# Dock Settings
-	system.defaults.dock.autohide = true;
-	system.defaults.dock.launchanim = false;
+{ pkgs, self, homebrew-core, homebrew-cask, ... }: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [ pkgs.vim pkgs.neovim pkgs.nixfmt ];
 
-	# Necessary for using flakes on this system.
-	nix.settings.experimental-features = "nix-command flakes";
+  homebrew = {
+    enable = true;
+    casks = [
+      { name = "firefox"; }
+      { name = "kitty"; }
+      { name = "visual-studio-code"; }
+      { name = "spotify"; }
+    ];
+  };
 
-	# Enable alternative shell support in nix-darwin.
-	# programs.fish.enable = true;
+  # Dock configuration
+  system.defaults.dock = {
+    autohide = true;
+    launchanim = false;
+    show-recents = false; # Don't show recent apps
+    tilesize = 48; # Icon size (default is 64)
+    magnification = false; # Enable magnification on hover
+    orientation = "bottom"; # Position: "bottom", "left", "right"
+    mineffect = "scale"; # Minimize effect: "genie", "scale", "suck"
+    show-process-indicators = true; # Show dots under running apps
+  };
 
-	# Set Git commit hash for darwin-version.
-	system.configurationRevision = self.rev or self.dirtyRev or null;
+  system.primaryUser = "dylan"; # Set main user for dock
 
-	# Used for backwards compatibility, please read the changelog before changing.
-	# $ darwin-rebuild changelog
-	system.stateVersion = 6;
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
-	# The platform the configuration will be used on.
-	nixpkgs.hostPlatform = "aarch64-darwin";
-    }
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  system.stateVersion = 6;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  nix-homebrew = {
+    enable = true;
+
+    enableRosetta = true;
+
+    user = "dylan";
+
+    taps = {
+      "homebrew/homebrew-core" = homebrew-core;
+      "homebrew/homebrew-cask" = homebrew-cask;
+    };
+    mutableTaps = false;
+  };
+}
 
