2572423271a22ca6d4efb26f577ca7f234be2e57
DylanSatow <dylansatow531@gmail.com>
2025-08-25 23:35:39 -0400

    Fixed hardware config

--- hosts/dylanpc/system.nix
+++ hosts/dylanpc/system.nix
@@ -4,6 +4,17 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
+
   networking.hostName = "dylanpc";
   networking.networkmanager.enable = true;
 
