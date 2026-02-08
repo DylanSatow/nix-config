4ba14fd080237e5d29fcb3b01fa96691042ec355
DylanSatow <dylansatow531@gmail.com>
2026-02-08 18:00:36 -0500

    added lsps

--- home/apps/nvim/lua/plugins/lsp.lua
+++ home/apps/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,31 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				nil_ls = {},
-				gopls = {},
-				pyright = {},
-				clangd = {},
-			},
-		},
-	},
+    -- {
+    -- 	"neovim/nvim-lspconfig",
+    -- 	opts = {
+    -- 		servers = {
+    -- 			nil_ls = {},
+    -- 			gopls = {},
+    -- 			pyright = {},
+    -- 			clangd = {},
+    -- 		},
+    -- 	},
+    -- },
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ensure_installed = {},
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = function(_, opts)
+            for server, server_opts in pairs(opts.servers) do
+                server_opts.mason = false
+            end
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+    },
 }
