d3db2082f82e7b8bf6663658b45424bd8b6b560f
DylanSatow <dylansatow531@gmail.com>
2026-04-13 13:00:23 -0400

    changed kitty font size

--- home/apps/kitty.nix
+++ home/apps/kitty.nix
@@ -1,27 +1,29 @@
-{ hostname, lib, ... } : {
-    programs.kitty = {
-        enable = true;
-        settings = {
-            font_family = "JetBrainsMono Nerd Font";
-            font_size = 12;
-            cursor_shape = "beam";
-            cursor_trail = 1;
+{ hostname, lib, ... }:
+{
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_family = "JetBrainsMono Nerd Font";
+      font_size = 20;
+      cursor_shape = "beam";
+      cursor_trail = 1;
 
-        } // lib.optionalAttrs (hostname == "dylanmac") {
-            macos_option_as_alt = "yes";
-        };
+    }
+    // lib.optionalAttrs (hostname == "dylanmac") {
+      macos_option_as_alt = "yes";
     };
-    programs.alacritty = {
-        enable = true;
-        settings = {
-            font = {
-                size = 11;
-                normal = {
-                    family = "JetBrains Mono Nerd Font";
-                    style = "Regular";
-                };
-            };
-            cursor.style.shape = "beam";
+  };
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 11;
+        normal = {
+          family = "JetBrains Mono Nerd Font";
+          style = "Regular";
         };
+      };
+      cursor.style.shape = "beam";
     };
+  };
 }
